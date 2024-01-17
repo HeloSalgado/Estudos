7ebabbce29d0e6660182cc89f279de330e2a7d53
HeloSalgado <heloisa.salgado@sptech.school>
2024-01-17 15:33:13 -0300

    Operações lógicas e array

---
+++ Curso Java/HelloWorld/src/domain/Aula1.java
@@ -0,0 +1,52 @@
+package domain;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Aula1 {
+    public static void main(String[] args) {
+        // AULA 4
+//        int valor1 = 10;
+//        int valor2 = 5;
+//
+//        int resultado;
+//        resultado = valor1 / valor2;
+//
+//        System.out.println(resultado);
+
+        // AULA 5
+//        int idade = 16;
+//        boolean resultado;
+//
+//        resultado = idade >= 18;
+//
+//        System.out.println(resultado);
+
+        // AULA 6
+//        int idade = 16;
+//
+//        if (idade >= 18){
+//            System.out.println("Já pode dirigir!");
+//        } else {
+//            System.out.println("Vai assistir desenho!");
+//        }
+
+        // AULA 7
+//        int opcao = 4;
+//
+//        if (opcao == 1){
+//            System.out.println("O saldo da sua conta é R$1.500,00 no momento");
+//        } else if (opcao == 2){
+//            System.out.println("O usuário cadastrado na conta é Helo");
+//        } else if (opcao == 3){
+//            System.out.println("Você será encaminhada para um atendente");
+//        } else {
+//            System.out.println("Opção não encontrada, por favor tente novamente!");
+//        }
+
+        // AULA 8
+//        String[] produto = {"Pacote Office", "Teams", "Word"};
+//
+//        System.out.println(produto[2]);
+    }
+}
