31a4686b90fb22007854f4f95dbf41430d82c9d3
HeloSalgado <heloisa.salgado@sptech.school>
2024-07-31 15:16:25 -0300

    Eventos, arrow function  e exercício

--- Curso React/ReactJS/05eventos/src/index.js
+++ Curso React/ReactJS/05eventos/src/index.js
@@ -3,12 +3,29 @@ import ReactDOM from 'react-dom';
 
 class MeuComponente extends React.Component{
     // Função
-    mensagem(){
-        alert('Hello World');
+    mensagem(nome){
+        alert('Olá ' + nome);
+        console.log(this);
+    }
+
+    minhaArrowFunction = (curso) => {
+        alert('Estou fazendo o curso de ' + curso);
+    }
+
+    teclado = (obj) => {
+        console.log(obj.target.value);
     }
 
     render(){
-        return <button onClick={this.mensagem}>Clique aqui</button>;
+        return(
+           <div>
+                <button onClick={this.mensagem.bind(this, 'Heloisa')}>Clique aqui</button>
+                <button onClick={() => this.minhaArrowFunction('ReactJS')}>Arrow Function</button>
+
+                <hr/>
+                <input type='text' onChange={this.teclado}/>
+           </div> 
+        ); 
     }
 }
 
---
+++ Curso React/ReactJS/06exercicio/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MeuComponente extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {texto: this.props.textoInicial}
+    }
+
+    minhaFuncao = (elemento) => {
+        this.setState({texto: elemento.target.value});
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>{this.state.texto}</h1>
+                <input type='text' onChange={this.minhaFuncao} value={this.state.texto}/>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <MeuComponente textoInicial='Digite algo...'/>,
+    document.getElementById('root')
+);
